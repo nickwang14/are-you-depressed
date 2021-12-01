0d9549e071fc2278109a122bf48955b1df42ab97
Nick Wang <nickwang14@gmail.com>
2021-12-01 02:05:45 -0500

    Updates

---
+++ precache-manifest.4226690f67065883b662a4119c8c0984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78edc6c7a9c5a2d2c0d0",
+    "url": "/are-you-depressed/static/css/main.b3b87b08.chunk.css"
+  },
+  {
+    "revision": "78edc6c7a9c5a2d2c0d0",
+    "url": "/are-you-depressed/static/js/main.47d0e6bf.chunk.js"
+  },
+  {
+    "revision": "149ffec102170e87d8d9",
+    "url": "/are-you-depressed/static/js/runtime~main.51495387.js"
+  },
+  {
+    "revision": "a85d5b484e9b98ffb034",
+    "url": "/are-you-depressed/static/js/2.e9607c72.chunk.js"
+  },
+  {
+    "revision": "526eef82ccef3ceae13027c3aedb74a4",
+    "url": "/are-you-depressed/index.html"
+  }
+];
