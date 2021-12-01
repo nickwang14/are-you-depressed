10b5d5f6f6c462e1234058eeed9c6b0f1a0860e6
Nick Wang <nickwang14@gmail.com>
2021-12-01 01:57:20 -0500

    Ship with build dist

---
+++ build/precache-manifest.1c30346dd0545286b0cadf0ee0d8ae58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0deae2db81985c8dfb",
+    "url": "/static/css/main.b3b87b08.chunk.css"
+  },
+  {
+    "revision": "4c0deae2db81985c8dfb",
+    "url": "/static/js/main.146c929c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9339a56c1883cf707d3",
+    "url": "/static/js/2.5e98ba4f.chunk.js"
+  },
+  {
+    "revision": "21a31f7952188bb78dd2c6233ba5d2f0",
+    "url": "/index.html"
+  }
+];
